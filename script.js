33b38f4125dca2003e6175fa9a3954e59238aa91
Rafael Assis Fontes <fael_assis@hotmail.com>
2022-03-28 10:32:28 -0300

    segundo forma de resolver o exercicio

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //solução 1
 
-function verificadorPalindromo(){
+/*function verificadorPalindromo(){
     let verificador = prompt("Digite uma palavra:")
     if(!verificador){
         alert(`Por favor digite uma palavra valida`);
@@ -14,4 +14,20 @@ function verificadorPalindromo(){
     }
 }
 
-verificadorPalindromo()
+verificadorPalindromo()*/
+
+function verificadorPalindromo2(){
+    let verificador = prompt("Digite uma palavra:")
+
+    if(!verificador) return alert("String inexistente")
+
+    for(let i = 0; i < verificador.length / 2; i++){
+        if(verificador[i] !== verificador[verificador.length -1 -i]){
+            return alert(`A palavra ${verificador} não é um palíndromo`)
+        }
+    }
+
+    return alert(`A palavra ${verificador} é um palíndromo`)
+}
+
+verificadorPalindromo2()
